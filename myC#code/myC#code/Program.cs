48da1e925e7a3f70c94b8b82e63adc5c6bd9e4f4
Hang Rithy <hangrithy001@gmial.com>
2024-01-08 22:05:47 +0700

    c sharp added

---
+++ myC#code/myC#code/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Threading.Tasks;
+
+namespace myC_code
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int option;
+            Console.WriteLine("1. Add Product");
+            Console.WriteLine("2. Show Product");
+
+            start:
+            Console.Write("Choose an option : ");
+            option = int.Parse(Console.ReadLine());
+
+            Student student = new Student();
+
+            switch (option) {
+                case 1:
+                    student.Add();
+                    break;
+                case 2:
+                    student.Show();
+                    break;
+                default:
+                    Console.WriteLine("Wrong Input Bitch!");
+                    break;
+            }
+            goto start;
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ myC#code/myC#code/Student.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using ConsoleTables;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myC_code
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Barcode { get; set; }
+        public float UnitPrice { get; set; }
+        public float SellPrice { get; set; }
+        public int QtyInstock { get; set; }
+
+        public int n;
+        public ArrayList arrayListStudent = new ArrayList();
+
+        public void Add()
+        {
+            
+            Console.Write("Enter N : ");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Student student = new Student();
+
+                Console.Write("Enter ID              : ");
+                student.Id = int.Parse(Console.ReadLine());
+                Console.Write("Enter Name            : ");
+                student.Name = Console.ReadLine();
+                Console.Write("Enter Barcode         : ");
+                student.Barcode = int.Parse(Console.ReadLine());
+                Console.Write("Enter Unit Price  : ");
+                student.UnitPrice = float.Parse(Console.ReadLine());
+                Console.Write("Enter Sell Price  : ");
+                student.SellPrice = float.Parse(Console.ReadLine());
+                Console.Write("Enter QTY Instock : ");
+                student.QtyInstock = int.Parse(Console.ReadLine());
+
+                arrayListStudent.Add(student);
+                Console.Clear();
+
+            }
+        }
+
+        public void Show()
+        {
+            var Table = new ConsoleTable();
+            string[] columnHeader = { "ID", "Name", "Barcode", "Unit Price", "Sell Price", "Qty Instock" };
+            for (int i = 0;i < arrayListStudent.Count; i++)
+            {
+                Student student = new Student();
+                student = (Student)arrayListStudent[i];
+
+                Table.AddRow(student.Id, student.Name, student.Barcode, student.UnitPrice, student.SellPrice, student.QtyInstock);
+            }
+        }
+    }
+}
